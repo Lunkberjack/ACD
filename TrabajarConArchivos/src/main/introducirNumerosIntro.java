8d4340a49656f375434828a7dbb501b581b9a988
Lunkubuntu <l.le.mi.lunk15@gmail.com>
2022-10-06 13:11:34 +0200

    Ejercicio espacios-may. RAF hecho

--- TrabajarConArchivos/src/main/introducirNumerosIntro.java
+++ TrabajarConArchivos/src/main/introducirNumerosIntro.java
@@ -1,5 +1,11 @@
 package main;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.Scanner;
 
 /**
@@ -11,12 +17,33 @@ import java.util.Scanner;
  */
 public class introducirNumerosIntro {
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		System.out.println("Introduce algo: ");
-		while((int)(sc.next().charAt(0)) != 13) {
-				System.out.print(sc.next().charAt(0));
-				System.out.println(sc.next().charAt(0));
-			sc.close();
+		File f = new File ("/home/lucia/Escritorio/numeros.txt");
+		Scanner scan = new Scanner(System.in);
+		DataOutputStream out;
+		DataInputStream in;
+		String[] numeros;
+		try {
+			numeros = new String[2];
+			out = new DataOutputStream(new FileOutputStream(f));
+			in = new DataInputStream(new FileInputStream(f));
+			// int var1, var2;
+			do {
+				System.out.println("Introduce dos números, separados por intro: ");
+				numeros[0] = scan.nextLine();
+				numeros[1] = scan.nextLine();
+				if(numeros[0].equals("") || numeros[1].equals("")) {
+					break;
+				}
+				//var1 = Integer.valueOf(numeros[0]);
+				//var2 = Integer.valueOf(numeros[1]);
+				out.writeChars(numeros[0]);
+				out.writeChar(' ');
+				out.writeChars(numeros[1]);
+				out.writeChar('\n');
+				} while (true);
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
+		scan.close();
 	}
 }
